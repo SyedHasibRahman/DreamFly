9ffd8a70b9c3f1c65625a4f1f6e91b1102d216d7
sohelrana22 <sohel252646@gmail.com>
2022-02-06 22:20:55 +0600

    booking start

---
+++ src/components/Booking/Book/Book.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Book = () => {
+    return (
+        <div>
+            <h2>This is book</h2>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/components/Booking/Booking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Booking = () => {
+    return (
+        <div>
+            <h2>This is booking</h2>
+        </div>
+    );
+};
+
+export default Booking;
