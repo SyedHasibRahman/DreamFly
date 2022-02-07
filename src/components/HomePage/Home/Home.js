6b2bf5043d472004918809c347bb40c7a548ff1a
Programmer Sajeeb <sajeeb.programmer@gmail.com>
2022-02-07 23:03:38 +0600

    Banner Section Complete

--- src/components/HomePage/Home/Home.js
+++ src/components/HomePage/Home/Home.js
@@ -1,10 +1,11 @@
+import { Box } from '@mui/material';
 import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <Box>
+            
+        </Box>
     );
 };
 
