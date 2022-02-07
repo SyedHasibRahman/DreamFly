b5214d4eaa589b6edd2d36f5d17ce23f7e5d2e07
alihaydarphpprogrammer <alihaydar6599@gmail.com>
2022-02-07 15:10:40 +0600

    veiw card push

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ function App() {
   return (
     <div className="App">
       Let Start Our Dream Project called DreamFly!
-<<<<<<< HEAD
-      {/* <h1>Hello</h1> */}
       <Home />
-
-=======
-      <Home/>
->>>>>>> 94c650c3d16f07c626d4bd91bcfcb1cf05843df7
     </div>
   );
 }
--- src/components/HomePage/Home/Home.js
+++ src/components/HomePage/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../About/About';
 import ServiceCards from '../ServiceCard/ServiceCards/ServiceCards';
+import ViewCounts from '../ViewCount/ViewCounts/ViewCounts';
 
 const Home = () => {
     return (
         <div>
             <About />
             <ServiceCards></ServiceCards>
+            <ViewCounts></ViewCounts>
         </div>
     );
 };
