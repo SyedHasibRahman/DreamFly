0a60967a259d466d9ceb2c501510f1493d6b9d4e
RajuM1997 <mraju5797@gmail.com>
2022-02-07 10:39:54 +0600

    added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import BestService from "./components/HomePage/BestService/BestService";
 
 function App() {
   return (
     <div className="App">
       <h1>Let Start Our Dream Project called DreamFly!</h1>
+      <BestService />
     </div>
   );
 }
