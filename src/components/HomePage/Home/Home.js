36831e65cf3f6075ab08bac2d414224b7f38f4b4
alihaydarphpprogrammer <alihaydar6599@gmail.com>
2022-02-15 00:14:54 +0600

    again push

--- src/components/HomePage/Home/Home.js
+++ src/components/HomePage/Home/Home.js
@@ -2,13 +2,13 @@ import { Box } from "@mui/material";
 import React from "react";
 import Booking from "../../Services/Booking/Booking";
 import TourPackages from "../../Shared/TourPackages/TourPackages";
-import Footer from "../../Shared/Footer/Footer";
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import BestService from "../BestService/BestService";
 import ServiceCards from "../ServiceCard/ServiceCards/ServiceCards";
 import Testimonials from "../Testimonial/Testimonials/Testimonials";
 import PersonalFlight from "../PersonalFlight/PersonalFlight";
+import NotFound from "../../Pages/NotFound/NotFound";
 
 
 const Home = () => {
@@ -17,6 +17,7 @@ const Home = () => {
       <Banner />
       <BestService />
       <About />
+      <NotFound></NotFound>
       <ServiceCards />
       <Booking></Booking>
       <PersonalFlight />
