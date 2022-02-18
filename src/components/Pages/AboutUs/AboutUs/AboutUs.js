794d726387ef0239a49b81684742544b60106569
Programmer Sajeeb <sajeeb.programmer@gmail.com>
2022-02-18 19:25:34 +0600

    some code update

--- src/components/Pages/AboutUs/AboutUs/AboutUs.js
+++ src/components/Pages/AboutUs/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
-import { Box } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
+import TestimonialSlider from '../../../HomePage/Testimonial/Slider/TestimonialSlider';
 import About from '../About/About';
 import CounterArea from '../CounterArea/CounterArea';
 import OurHistory from '../OurHistory/OurHistory';
@@ -14,6 +15,15 @@ const AboutUs = () => {
             <CounterArea />
             <OurHistory />
             <OurTeam />
+            <Box sx={{padding:'50px 0'}}>
+            <Container>
+                <Box>
+                <Typography variant="h4" component="h2" sx={{fontWeight:'700', fontSize:'20px', marginBottom:'20px', fontFamily:'"Mulish", sans-serif', color:'#dcbb87', lineHeight:'1.5em', textAlign:'center'}}> Our team </Typography>
+                <Typography variant="h3" component="h2" sx={{fontWeight:'700', fontSize:'40px', lineHeight:'1.3em', color:'#19232d', fontFamily:'"Playfair Display", sans-serif', textAlign:'center'}}> Meet Our Team Members </Typography>
+                </Box>
+            <TestimonialSlider />
+            </Container>
+            </Box>
         </Box>
     );
 };
