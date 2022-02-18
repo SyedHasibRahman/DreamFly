5a06e48e0a6c497ea5299b113651668b800d3d45
sohelrana22 <sohel252646@gmail.com>
2022-02-18 20:31:59 +0600

    data added

--- src/components/Pages/Services/Services/Services.js
+++ src/components/Pages/Services/Services/Services.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Booking from '../../../Services/Booking/Booking';
 
 const Services = () => {
     return (
         <div>
             <h1>Services</h1>
+            <Booking></Booking>
         </div>
     );
 };
