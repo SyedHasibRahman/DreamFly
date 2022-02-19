f77ae6053baa78370cdfc62b5bcdc16f975c6ded
muhammadnowshad <muhammadnowshad2020@gmail.com>
2022-02-19 21:16:50 +0600

    blog comment section add

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/HomePage/Home/Home";
-import SignInSide from "./components/Pages/Login/SingIn/SignInSide";
-import SingUp from "./components/Pages/Login/SingUp/SingUp";
 import Blogs from "./components/Pages/Blogs/Blogs/Blogs";
 import ContactUs from "./components/Pages/ContactUs/ContactUs";
 import OurPilots from "./components/Pages/OurPilots/OurPilots/OurPilots";
@@ -13,10 +11,12 @@ import PrivateRoute from "./components/Pages/Login/OtherRoutes/PrivateRoute/Priv
 import Sidebar from "./components/Dashboard/Sidebar/Sidebar";
 import NotFound from "./components/Pages/NotFound/NotFound";
 import BlogDetails from "./components/Pages/Blogs/BlogDetails/BlogDetails";
-import AddPost from "./components/Dashboard/Admin/AddPost/AddPost";
+import AddPost from "./components/Dashboard/Admin/Add/AddPost";
 import AboutUs from "./components/Pages/AboutUs/AboutUs/AboutUs";
 import OurCoursesDetails from "./components/Pages/OurCourses/OurCoursesDetails/OurCoursesDetails";
 import OurCourses from "./components/Pages/OurCourses/OurCourses";
+import SignUp from "./components/Pages/Login/SignUp/SignUp";
+import SignInSide from "./components/Pages/Login/SignIn/SignInSide";
 
 function App() {
   return (
@@ -37,7 +37,7 @@ function App() {
           <Route path="/OurCoursesDetails" element={<OurCoursesDetails />} />
           <Route path="/OurCourses" element={<OurCourses />} />
           <Route path="SignIn" element={<SignInSide />} />
-          <Route path="SingUp" element={<SingUp />} />
+          <Route path="SignUp" element={<SignUp />} />
           <Route
             path="Dashboard"
             element={
