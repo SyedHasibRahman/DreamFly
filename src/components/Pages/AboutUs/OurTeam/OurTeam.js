ce881181ebdce0df8250d85bf5e1ef7acb0c59a0
alihaydarphpprogrammer <alihaydar6599@gmail.com>
2022-03-18 12:51:17 +0600

    done

--- src/components/Pages/AboutUs/OurTeam/OurTeam.js
+++ src/components/Pages/AboutUs/OurTeam/OurTeam.js
@@ -1,4 +1,4 @@
-import {  Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { Box } from '@material-ui/system';
 import React from 'react';
@@ -6,40 +6,51 @@ import SecondaryButton from '../../../StyledComponent/Buttons/SecondaryButton';
 import './OurTeam.css';
 
 const OurTeam = (props) => {
-  const { _id, name, role, img, mobile, email, facebook, twitter, instragram, website, linkedin } = props.ourTeam;
+  const { _id, name, role, img, facebook, twitter, instragram, linkedin } = props.ourTeam;
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', marginRight:'10px'}}>
-                  <Box className="team-member" sx={{ backgroundColor: '#f5f3f1', borderRadius: '5px', width: '350px', position:'relative' }}>
-                    <Box 
-                      sx={{ display: 'flex', justifyContent: 'center', borderRadius: '5px 5px 5px 5px' }}
-                    >
-                      <Box
-                        sx={{ width: '100%', height: '370px', backgroundColor: '#DDE6EF', borderRadius: '5px 5px 0 0 ' }}
-                      >
-                        <img style={{ width: '100%', height: '100%', objectFit: 'fill' }} src={img} alt="" />
-                      </Box>
-                    </Box>
-                    <Typography
-                      sx={{ backgroundColor: 'transparent', fontFamily: '"Mulish", sans-serif', fontWeight: '800', fontSize: "24px", textAlign: 'center', paddingTop: '16px', margin: '0', color: '#19232d', }}
-                    >
-                      {name}
-                    </Typography>
-                    <Typography
-                      sx={{ textAlign: 'center', fontFamily: '"Mulish", sans-serif', fontSize: '18px', fontWeight: '700', fontStyle: 'italic', margin: '0', paddingBottom: '16px', color: '#19232d' }}
-                    >
-                      {role}
-                    </Typography>
-                    <Link to={`/TeamDetails/${_id}`}><SecondaryButton>Details</SecondaryButton></Link>
-                    <Box className="teram-social" sx={{position:'absolute', right:'-40px', top:'0'}}>
-                    <Box sx={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', height:'500px'}}>
-                    <Link to={facebook}><i class="fab fa-facebook-f"></i></Link>
-                    <Link to={twitter}><i class="fab fa-twitter"></i></Link>
-                    <Link to='#'><i class="fab fa-youtube"></i></Link>
-                    <Link to={instragram}><i class="fab fa-instagram"></i></Link>
-                    </Box>
-                </Box>
-              </Box>
-           </Box>
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginRight: '10px' }}>
+      <Box className="team-member" sx={{ backgroundColor: '#f5f3f1', borderRadius: '5px', width: '350px', position: 'relative' }}>
+        <Box
+          sx={{ display: 'flex', justifyContent: 'center', borderRadius: '5px 5px 5px 5px' }}
+        >
+          <Box
+            sx={{ width: '100%', height: '370px', backgroundColor: '#DDE6EF', borderRadius: '5px 5px 0 0 ' }}
+          >
+            <img style={{ width: '100%', height: '100%', objectFit: 'fill' }} src={img} alt="" />
+          </Box>
+        </Box>
+        <Typography
+          sx={{ backgroundColor: 'transparent', fontFamily: '"Mulish", sans-serif', fontWeight: '800', fontSize: "24px", textAlign: 'center', paddingTop: '16px', margin: '0', color: '#19232d', }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{ textAlign: 'center', fontFamily: '"Mulish", sans-serif', fontSize: '18px', fontWeight: '700', fontStyle: 'italic', margin: '0', paddingBottom: '8px', color: '#19232d' }}
+        >
+          {role}
+        </Typography>
+        <Typography
+          sx={{ textAlign: 'center', fontFamily: '"Mulish", sans-serif', fontSize: '18px', fontWeight: '700', fontStyle: 'italic', margin: '0', paddingBottom: '10px', color: '#19232d' }}
+        >
+          <Link to={`/TeamDetails/${_id}`} style={{ textDecoration: 'none' }}><SecondaryButton>Details</SecondaryButton></Link>
+        </Typography>
+        <Box className="teram-social" sx={{ position: 'absolute', right: '-61px', top: '0' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '500px' }}>
+            <a href={facebook} className='social-icon-box my-2' target='_blank' rel="noreferrer">
+              <i className="fab fa-facebook-f social-icon"></i>
+            </a>
+            <a href={twitter} className='social-icon-box my-2' target='_blank' rel="noreferrer">
+              <i className="fab fa-twitter social-icon"></i></a>
+            <a href={linkedin} sx={{}} className='social-icon-box my-2' target='_blank' rel="noreferrer">
+              <i className="fab fa-linkedin social-icon"></i>
+            </a>
+            <a href={instragram} sx={{}} className='social-icon-box my-2' target='_blank' rel="noreferrer">
+              <i className="fab fa-instagram social-icon"></i>
+            </a>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- src/components/Pages/OurCourses/OurCourses.js
+++ src/components/Pages/OurCourses/OurCourses.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -15,64 +15,15 @@ import {
 } from "@mui/material";
 import Footer from "../../Shared/Footer/Footer";
 import SecondaryButton from "../../StyledComponent/Buttons/SecondaryButton";
+import { Link } from "react-router-dom";
 
 const OurCourses = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Private Pilot License (PPL)",
-      hours: "45 Hours",
-      dec: "As well as getting to fly to many different destinations as part of their job, airplane pilots get big discounts on",
-      images:
-        "https://images.unsplash.com/photo-1506695939086-156c2eff767b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGxhbmUlMjBjb2NrcGl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      price: "399",
-    },
-    {
-      id: 2,
-      title: "Commercial Pilot License",
-      hours: "35 Hours",
-      dec: "The first small jet-powered civil aircraft was the Morane-Saulnier MS.760 Paris, developed privately in the early 1950s",
-      images:
-        "https://images.unsplash.com/photo-1529521914207-a330038e35a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGxhbmUlMjBjb2NrcGl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      price: "299",
-    },
-    {
-      id: 3,
-      title: "Multi-Engine IFR Rating",
-      hours: "40 Hours",
-      dec: "Our technology consistently delivers the best pricing for charters – and the unique ability to buy individual seats.",
-      images:
-        "https://images.unsplash.com/photo-1596899223071-723a26d67f9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBsYW5lJTIwY29ja3BpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      price: "499",
-    },
-    {
-      id: 4,
-      title: "Private Pilot License (PPL)",
-      hours: "30 Hours",
-      dec: "Search the world with ease and transparency.As the only tech-forward private aviation company",
-      images:
-        "https://images.unsplash.com/photo-1581300907482-9ab70b31b69f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHBsYW5lJTIwY29ja3BpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      price: "199",
-    },
-    {
-      id: 5,
-      title: "Commercial Pilot License",
-      hours: "25 Hours",
-      dec: "Charter an entire jet, or offer the seats you don’t need through our app a need for full or even fractional jet ownership",
-      images:
-        "https://images.unsplash.com/photo-1610081206051-a73bc07abf23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHBsYW5lJTIwY29ja3BpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      price: "599",
-    },
-    {
-      id: 6,
-      title: "Multi-Engine IFR Rating",
-      hours: "20 Hours",
-      dec: "XO is not simply a private jet service. Whether you’re traveling for business or leisure, XO Membership also delivers",
-      images:
-        "https://images.unsplash.com/photo-1509541206217-cde45c41aa6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fHBsYW5lJTIwY29ja3BpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      price: "699",
-    },
-  ];
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then(res => res.json())
+      .then((data) => setData(data))
+  }, [])
   return (
     <Box>
       <Navigation />
@@ -93,7 +44,7 @@ const OurCourses = () => {
             </Typography>
           </Box>
           <Grid container spacing={{ xs: 1, md: 3 }}>
-            {data.map((item) => (
+            {data.map((item, _id) => (
               <Grid item xs={12} sm={6} md={4} key={item.id}>
                 <Card
                   sx={{
@@ -144,7 +95,9 @@ const OurCourses = () => {
                     </CardContent>
                     <CardActions sx={{ pb: 2 }}>
                       <SecondaryButton>
-                        Enroll Now
+                        <Link to={`/Book/${_id}`} style={{ textDecoration: 'none', color: 'pink' }}>
+                          Book Now
+                        </Link>
                         <span className="course_arrow">
                           <i className="fas fa-arrow-right"></i>
                         </span>
